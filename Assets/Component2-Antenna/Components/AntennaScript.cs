7fc07135516432a85ce0b2132048c2b756fa0dfb
Yoan Allibert <yoan.allibert@gmail.com>
2020-02-26 00:56:16 +0000

    Comp2

---
+++ Assets/Component2-Antenna/Components/AntennaScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SelectionBase]//Diverts the selection to this object
+public class AntennaScript : MonoBehaviour
+{
+    [Header("Physics Parameters")]
+    [SerializeField] private float springForce = 80.0f;
+    [SerializeField] private float drag = 2.5f;
+    
+    
+	[Space(12)]
+    public Transform GeoParent;
+
+    Rigidbody SpringRB;
+    private Vector3 LocalDistance;//Distance between the two points
+    private Vector3 LocalVelocity;//Velocity converted to local space
+    public Transform springTarget;
+    public Transform springObj;
+
+    void Start()
+    {
+        SpringRB = springObj.GetComponent<Rigidbody>();//Find the RigidBody component
+        springObj.transform.parent = null;//Take the spring out of the hierarchy
+    }
+
+    void FixedUpdate()
+    {
+        //Sync the rotation 
+        SpringRB.transform.rotation = this.transform.rotation;
+
+        //Calculate the distance between the two points
+        LocalDistance = springTarget.InverseTransformDirection(springTarget.position - springObj.position);
+        SpringRB.AddRelativeForce((LocalDistance) * springForce);//Apply Spring
+
+        //Calculate the local velocity of the springObj point
+        LocalVelocity = (springObj.InverseTransformDirection(SpringRB.velocity));
+        SpringRB.AddRelativeForce((-LocalVelocity) * drag);//Apply drag
+
+        //Aim the visible geo at the spring target
+        GeoParent.transform.LookAt(springObj.position);
+        GeoParent.transform.Rotate(90f, 0f, 0f);
+    }
+}
---
+++ Assets/Component3-Rotation/Components/RotationBody.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationBody : MonoBehaviour
+{
+    [SerializeField] private bool clockwise;
+
+    private Rigidbody rb;
+
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
