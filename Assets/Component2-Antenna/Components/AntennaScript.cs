504451aa94a3ca9a2b764d35fcc044f8a19e0b21
Yoan Allibert <yoan.allibert@gmail.com>
2020-02-26 01:55:47 +0000

    LineR

--- Assets/Component2-Antenna/Components/AntennaScript.cs
+++ Assets/Component2-Antenna/Components/AntennaScript.cs
@@ -8,10 +8,11 @@ public class AntennaScript : MonoBehaviour
     [Header("Physics Parameters")]
     [SerializeField] private float springForce = 80.0f;
     [SerializeField] private float drag = 2.5f;
-    
+    public float lenght;
     
 	[Space(12)]
-    public Transform GeoParent;
+    //public Transform GeoParent;
+    //public Transform endPosition;
 
     Rigidbody SpringRB;
     private Vector3 LocalDistance;//Distance between the two points
@@ -19,14 +20,22 @@ public class AntennaScript : MonoBehaviour
     public Transform springTarget;
     public Transform springObj;
 
+    private LineRenderer lineRenderer;
+
     void Start()
     {
         SpringRB = springObj.GetComponent<Rigidbody>();//Find the RigidBody component
+        lineRenderer = GetComponent<LineRenderer>();
         springObj.transform.parent = null;//Take the spring out of the hierarchy
     }
 
     void FixedUpdate()
     {
+        //endPosition.position = springObj.position;
+        Vector3 endPoint = (SpringRB.position - transform.localPosition).normalized;
+        endPoint *= lenght;
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, transform.position + endPoint);
         //Sync the rotation 
         SpringRB.transform.rotation = this.transform.rotation;
 
@@ -39,7 +48,7 @@ public class AntennaScript : MonoBehaviour
         SpringRB.AddRelativeForce((-LocalVelocity) * drag);//Apply drag
 
         //Aim the visible geo at the spring target
-        GeoParent.transform.LookAt(springObj.position);
-        GeoParent.transform.Rotate(90f, 0f, 0f);
+        //GeoParent.transform.LookAt(springObj.position);
+        //GeoParent.transform.Rotate(90f, 0f, 0f);
     }
 }
